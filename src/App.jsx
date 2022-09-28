28d3e3df49d409b2181ff996a8972b2031e49396
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-28 10:08:21 -0300

    consulta a la api agregada

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import "./App.css";
 import Frase from "./components/Frase";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(()=>{
+    //Esta funcion solo se ejecuta en el montaje 
+    consultarApi();
+  }, []);
+
+  const consultarApi = async () => {
+    try {
+      //Aqui hago la peticion o request
+      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const dato = await respuesta.json();
+      //Guardar la frase del personaje en el state
+      setPersonaje(dato[0]);
+
+    } catch (error) {
+      //Muestra un msj de error en el caso de que exista
+      console.log(error);
+    }
+  }
+
   return (
     <section className="container">
       <article >
@@ -12,7 +34,7 @@ function App() {
       <article className="text-center">
       <Button variant="warning">Obtener frase</Button>
       </article>
-      <Frase></Frase>
+      <Frase personaje={personaje}></Frase>
     </section>
   );
 }
