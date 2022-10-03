83c8376050b686b75bb7541ce5e7ebaf1081c399
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-10-03 10:04:42 -0300

    Agregamos un componente condicional y mostramos o el spinner o el componente frase

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,54 @@ import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import "./App.css";
 import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
-  useEffect(()=>{
-    //Esta funcion solo se ejecuta en el montaje 
+  useEffect(() => {
+    //Esta funcion solo se ejecuta en el montaje
     consultarApi();
   }, []);
 
   const consultarApi = async () => {
     try {
+      setMostrarSpinner(true)
       //Aqui hago la peticion o request
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
       const dato = await respuesta.json();
       //Guardar la frase del personaje en el state
       setPersonaje(dato[0]);
-
+      //Cambiar el valor de mostrarSpinner para que se vea el componente Frase
+      setMostrarSpinner(false);
     } catch (error) {
       //Muestra un msj de error en el caso de que exista
       console.log(error);
     }
-  }
+  };
+
+  //Cargar un componente condicional
+  const componenteCondicional = mostrarSpinner ? (
+    <Spinner></Spinner>
+  ) : (
+    <Frase personaje={personaje}></Frase>
+  );
 
   return (
-    <section className="container">
-      <article >
-        <img className="w-100" src={require("./img/titulo.png")} alt="" />
+    <Container>
+      <article className="text-center">
+        <img className="w-50" src={require("./img/titulo.png")} alt="" />
       </article>
       <article className="text-center">
-      <Button variant="warning">Obtener frase</Button>
+        <Button variant="warning" onClick={consultarApi}>
+          Obtener frase
+        </Button>
       </article>
-      <Frase personaje={personaje}></Frase>
-    </section>
+      {componenteCondicional}
+    </Container>
   );
 }
 
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import { Container } from 'react-bootstrap';
 
-const Frase = () => {
+const Frase = ({personaje}) => {
     return (
         <Container className='bg-white rounded my-5 py-4 d-flex justify-content-evenly'>
             <article >
-            <img src={require("./../img/homero.png")} alt=""  className='w-100'/>
+            <img src={personaje.image} alt={personaje.character}  className='w-100'/>
             </article>
             <article className='text-start me-5'>
-            <h4 className='fw-bold'>Homer Simpson</h4>
-            <p>Facts are miningless. You could use facts to prove anything thats even remotely true.</p>
+            <h4 className='fw-bold'>{personaje.character}</h4>
+            <p>{personaje.quote}</p>
             </article>
         </Container>
     );
